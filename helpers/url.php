c7667f48a6b07c9b8deb0cd0816b69c3e6fca1bb
tiago <sawadeekrap1@gmail.com>
2022-09-07 20:20:39 -0300

    refatoração de codigo

--- helpers/url.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-$BASE_URL = $_SERVER['REQUEST_SCHEME'] . "://" . $_SERVER['SERVER_NAME'] . dirname($_SERVER['REQUEST_URI'] . '?') . '/';
-
-//var_dump($_SERVER);
-
--- templates/footer.php
+++
@@ -1,8 +0,0 @@
-<?php
-  include_once("<templates/header.php");
-?>
-<footer>
-  <p>Hora de Codar &copy; 2020</p>
-</footer>
-</body>
-</html>
